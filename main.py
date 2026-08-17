80c06218ca5817f8a2561ab3f0675b15b4446dce
FRJ74 <frankr74@gmail.com>
2026-08-17 12:55:45 -0600

    Add variables and logic

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 #This is a Travel Weather Planner that provides weather information for different travel destinations.
+distance_mi = 1
+is_raining = True
+has_bike = True
+has_car = True
+has_ride_share_app = True
 
+if not distance_mi:
+    print(False)
+
+elif distance_mi <= 1:
+    if is_raining:
+        print(False)
+    else:
+        print(True)
+
+elif distance_mi <= 6:
+    if has_bike and not is_raining:
+        print(True)
+    else:
+        print(False)
+
+else:
+    if has_car or has_ride_share_app:
+        print(True)
+    else:
+        print(False)
